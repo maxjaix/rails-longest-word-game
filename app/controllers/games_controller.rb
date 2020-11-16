35403fa3e5fd5741d1c979d71272221e52ef0421
maxime jourdan <maximejourdan1@gmail.com>
2020-11-16 16:54:38 +0100

    exo3

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,11 @@
+class GamesController < ApplicationController
+  def new
+    @letters = []
+    10.times do
+      @letters << ("a".."z").to_a.sample.capitalize
+    end
+  end
+
+  def score
+  end
+end
